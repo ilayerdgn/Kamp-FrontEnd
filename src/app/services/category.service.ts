022ed86e6f7efbb2bb9d58d360ca8d1beafd8aea
ilayerdgn <ilayda.erdogan@gazi.edu.tr>
2021-05-13 00:51:03 +0300

    commit 2

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -11,7 +11,7 @@ import { Category } from '../models/category';
 })
 export class CategoryService {
 
-  apiUrl="https://localhost:48952/api/categories/getall"
+  apiUrl="https://localhost:48952/api/categories/getall";
   constructor(private httpClient:HttpClient) {}
   getCategories():Observable<ListResponseModel<Category>>{
     return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
